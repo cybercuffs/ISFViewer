7dc4d65106d44717d92541b0c3b64dcfd9a7b7d9
cybercuffs <cybercuffs@yahoo.com>
2015-03-29 17:48:27 -0700

    Windows ISF file viewer

---
+++ ISFViewertestForm/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ISFViewertestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(76, 357);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load ISF file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(336, 357);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Load ISF folder";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(230, 433);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F);
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(398, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "By Nasa and Khizra";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(90, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 148);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(243, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(562, 505);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "ISF Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ISFViewertestForm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Ink;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Ink;
+
+
+
+namespace ISFViewertestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Select the source file
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "ISF files (*.isf)|*.isf";
+            openFileDialog1.Title = "Browse an ISF file to be converted";
+            DialogResult result = openFileDialog1.ShowDialog();
+            
+
+            if (result == DialogResult.OK)
+            {
+
+                // Converting ISF file to stream fs
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, (int)bytes.Length);
+
+                // Loading Ink from 
+                InkOverlay mInkOverlay = new InkOverlay();
+                mInkOverlay.Ink.Load(bytes);
+                fs.Write(bytes, 0, bytes.Length);
+
+                // Saving Ink in GIF format inside a byte array;
+                byte[] gif = mInkOverlay.Ink.Save(PersistenceFormat.Gif);
+                fs.Write(gif, 0, gif.Length);
+
+                // Dialog box to save the converted GIF file to the desired location
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "Gif Image|*.gif";
+                saveFileDialog1.Title = "Save an Image File";
+                saveFileDialog1.ShowDialog();
+
+                // Writing bytes to the file
+                System.IO.File.WriteAllBytes(saveFileDialog1.FileName, gif);
+
+                //Print Done! when the processing is completed
+                label2.Text = "Done!";
+
+            }               
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Select a ISF source folder
+            FolderBrowserDialog openFolderDialog1 = new FolderBrowserDialog();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFolderDialog1.Description = "Browse an ISF folder";
+            DialogResult result = openFolderDialog1.ShowDialog();
+
+            // Select destination folder
+            FolderBrowserDialog saveFolderDialog1 = new FolderBrowserDialog();
+            saveFolderDialog1.Description = "Browse destination folder";
+            DialogResult output = saveFolderDialog1.ShowDialog();
+           
+
+            if (result == DialogResult.OK)
+            {
+                System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(openFolderDialog1.SelectedPath);
+                int count = dir.GetFiles().Length;
+                          
+                if (count != 0)
+                {
+                    foreach (string file in Directory.EnumerateFiles(openFolderDialog1.SelectedPath, "*.isf"))
+                    {
+
+                        //String myfilename = Path.GetFileName(file);
+
+                        // Converting ISF file to stream fs
+                        FileStream fs = new FileStream(file, FileMode.Open);
+                        byte[] bytes = new byte[fs.Length];
+                        fs.Read(bytes, 0, (int)bytes.Length);
+
+                        // Loading Ink from 
+                        InkOverlay mInkOverlay = new InkOverlay();
+                        mInkOverlay.Ink.Load(bytes);
+                        fs.Write(bytes, 0, bytes.Length);
+
+                        // Saving Ink in GIF format inside a byte array;
+                        byte[] gif = mInkOverlay.Ink.Save(PersistenceFormat.Gif);
+                        fs.Write(gif, 0, gif.Length);
+
+                        //Saving files with the same name as ISF and location specified in the dialog box of destination folder
+                        var tempfilenameandlocation = Path.Combine(saveFolderDialog1.SelectedPath, Path.GetFileNameWithoutExtension(file));
+                        String ext = ".gif";
+                        System.IO.File.WriteAllBytes(tempfilenameandlocation + ext, gif);
+
+                        //Print Done! when the processing is completed
+                        label2.Text = "Done!";
+                    }
+                }              
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
